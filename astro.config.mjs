7e2813aad461e44519da028ac69c1deef697ec11
WessellUrdata <git@wessell.dev>
2024-05-07 04:42:39 +0800

    Adding fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,12 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      sans: ["Noto Sans Variable", "Noto Sans HK Variable", ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   daisyui: {
@@ -25,5 +30,5 @@ export default {
     styled: true,
     utils: true,
   },
-  plugins: [require("daisyui"), require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
