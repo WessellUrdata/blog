b98eb70932afa2b9f259f1e920c2e168144332f0
WessellUrdata <git@wessell.dev>
2024-05-07 07:52:04 +0800

    even more progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@ import tailwind from "@astrojs/tailwind";
 
 // import cloudflare from "@astrojs/cloudflare";
 
+import mdx from "@astrojs/mdx";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   // output: "server",
   // adapter: cloudflare()
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
