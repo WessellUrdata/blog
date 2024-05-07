094f10ba1cee465cc8a09be67159b3658d2c69ad
WessellUrdata <git@wessell.dev>
2024-05-08 03:59:14 +0800

    Added robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,19 +7,21 @@ import mdx from "@astrojs/mdx";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  site: "https://wesl.cc",
+  integrations: [tailwind(), mdx(), sitemap()],
   // output: "server",
   // adapter: cloudflare()
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
-
     // for syntax highlighting
     shikiConfig: {
       // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-    },
-  },
+      wrap: true
+    }
+  }
 });
